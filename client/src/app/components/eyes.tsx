886a440ad293e62b593e063cc26ad42037fdf3c0
JustinDo2909 <hieudeptraivodichh@gmail.com>
2025-01-25 18:54:37 +0700

    chinh sua footer

--- client/src/app/components/eyes.tsx
+++ client/src/app/components/eyes.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React, { useEffect } from "react";
-
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import tear from '@/app/asset/img/drop.png'
 const Eyes = () => {
+  const [happy, setHappy] = useState(false);
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       const { clientX, clientY } = e;
@@ -38,21 +40,45 @@ const Eyes = () => {
     };
   }, []);
 
-  return (
-    <div className="flex flex-col justify-center items-center border rounded-full bg-white-50  shadow-xl py-3">
-  <div className="flex">
-    <div className="eye bg-white rounded-full border-4 border-black w-20 h-20 mb-9 flex justify-center items-center m-4 relative overflow-hidden">
-      <div className="pupil bg-black rounded-full w-8 h-8 absolute"></div>
-    </div>
-    <div className="eye bg-white rounded-full border-4 border-black w-20 h-20 mb-9 flex justify-center items-center m-4 relative overflow-hidden">
-      <div className="pupil bg-black rounded-full w-8 h-8 absolute"></div>
-    </div>
-  </div>
-  <div className="flex justify-center items-center text-5xl font-bold z-10 mt-4 rotate-90 text-black">
-    (
-  </div>
-</div>
+  const handleOnClick = () => {
+    setHappy(!happy);
+  };
 
+  return (
+    <div
+      onClick={() => handleOnClick()}
+      className={`flex flex-col justify-center items-center border rounded-full transition-all ${
+        happy
+          ? "bg-pink-500 duration-700 transition-colors"
+          : "bg-white-50  duration-700 transition-colors px-4"
+      }   shadow-xl py-3 `}
+    >
+      <div className="flex">
+        <div className="eye bg-white rounded-full border-4 border-black w-20 h-20 mb-9 flex justify-center items-center m-4 relative overflow-hidden">
+          <div className="pupil bg-black rounded-full w-8 h-8 absolute"></div>
+        </div>
+        <div className="flex flex-col">
+          <div className="eye bg-white rounded-full border-4 border-black w-20 h-20 mb-9 flex justify-center items-center m-4 relative overflow-hidden">
+            <div className="pupil bg-black rounded-full w-8 h-8 absolute"></div>
+          </div>
+          <div className={` ml-9 -translate-y-8 transition-all ${happy ? "hidden  transition-opacity duration-300 opacity-0 " : 'visible transition-opacity duration-300 opacity-100'}`}>
+            <Image
+              src={tear}
+              height={100}
+              width={30}
+              alt="ok"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        className={`flex justify-center items-center text-5xl font-bold z-10 mt-4  ${
+          happy ? "-rotate-90 " : "rotate-90"
+        }  text-black`}
+      >
+        (
+      </div>
+    </div>
   );
 };
 
